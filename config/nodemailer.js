4fbbe2bb79f24e433d64334d7a8fefa920b9d7a3
Anjali Patil <anjalipatil3993@gmail.com>
2025-05-29 17:56:55 +0530

    nodemailer updated

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,10 +1,16 @@
-const nodemailer = require('nodemailer')
+const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-    service:'gmail',
-    auth:{
-        user:process.env.EMAIL_USER,
-        pass:process.env.EMAIL_PASS,
-    },
-})
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true, // Use SSL/TLS
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  // Optional: Enable debugging for detailed logs
+  debug: true,
+  logger: true,
+});
+
 module.exports = transporter;
